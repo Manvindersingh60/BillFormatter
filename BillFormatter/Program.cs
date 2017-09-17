713cf899923f1c0ea546e3fc72e5082b74599424
Manvinder Singh <Manvinder Singh@MANVINDER>
2017-09-17 18:11:19 +0530

    Initial working commit
    Initial working commit

--- BillFormatter/Program.cs
+++ BillFormatter/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
+using System.Configuration;
 
 namespace BillFormatter
 {
@@ -9,23 +11,68 @@ namespace BillFormatter
     {
         static void Main(string[] args)
         {
-            PdfReader reader=new PdfReader(@"E:\asd.pdf");
+            string[] files = Directory.GetFiles(ConfigurationManager.AppSettings["Source"]);
 
+            foreach (var file in files)
+            {
+                FormatFile(file);
+                Console.WriteLine(file);
+            }
+            Console.WriteLine("Done");
+            Console.Read();
+        }
+
+        public static  void FormatFile(string fileName)
+        {
+
+            PdfReader reader = new PdfReader(fileName);
             ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
             string currentText = PdfTextExtractor.GetTextFromPage(reader, 1, strategy);
 
             string text = Encoding.UTF8.GetString(Encoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
-            int x=text.IndexOf("Bill Details");
-            int y = text.IndexOf("2016");
-            string z = text.Substring(x+13, y-x-15);
-            z = z.Replace(" ", "_");
-            if (text.Contains(("AM"))) ;
+            //Console.Write(text);
+            //Console.Read();
+            //int x=text.IndexOf("Bill Details");
+            int yearIndex = text.LastIndexOf("2016", StringComparison.Ordinal);
+            if (yearIndex < 0)
             {
-                z = z + "_Morning";
+                yearIndex = text.LastIndexOf("2017", StringComparison.Ordinal);
             }
-            Console.Write(text);
-            //int x = reader.NumberOfPages;
-            Console.Read();
+            string date = text.Substring(yearIndex - 8, 6);
+            date = date.Replace(" ", "_");
+            if (text.Contains(("AM")))
+            {
+                date = date + "_Morning";
+
+            }
+            else
+            {
+                int timeIndex = text.IndexOf("PM");
+                string rideStartHour = text.Substring(timeIndex - 6, 2);
+                rideStartHour=rideStartHour.Trim();
+                int time = Convert.ToInt16(rideStartHour);
+                if (time < 2||time==12)
+                {
+                    date = date + "_Morning";
+                }
+
+                else
+                {
+                    date = date + "_Evening";
+                }
+            }
+            reader.Dispose();
+            string newFile = @"E:\bills\march-april\" + date + ".pdf";
+            if (!File.Exists(newFile))
+            {
+                File.Copy(fileName, newFile); 
+            }
+            else
+            {
+                Console.WriteLine(fileName+"   "+newFile);
+                File.Copy(fileName, @"E:\bills\dublicate_march-april\" + date + ".pdf");
+            }
+            //File.Move(fileName, @"E:\"+date+".pdf");
         }
     }
 }
